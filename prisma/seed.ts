f0220b6e89cb0bea68fdd4f53c53b935ccacec52
AutoScreencast <tom.bowden@autoscreencast.com>
2022-05-31 14:21:51 +0900

    Remove Note model, update User model, update seed, and add secret users reference to .gitignore

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
+import { USERS } from "./secrets/users";
+
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
-
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
-
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
+  USERS.forEach(
+    async (user) =>
+      await prisma.user.create({
+        data: {
+          name: user.name,
+          username: user.username,
+          password: {
+            create: {
+              hash: await bcrypt.hash(user.password, 10),
+            },
+          },
         },
-      },
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+      })
+  );
 
   console.log(`Database has been seeded. 🌱`);
 }
