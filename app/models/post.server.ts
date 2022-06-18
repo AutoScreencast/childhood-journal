cfc223090a12c33dc47f3af4effe93de9839678d
AutoScreencast <tom.bowden@autoscreencast.com>
2022-06-19 07:13:24 +0900

    feature: group posts by month

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -5,6 +5,8 @@ import {
   formatISO,
   parseISO,
 } from "date-fns";
+import head from "lodash.head";
+import invariant from "tiny-invariant";
 
 import { prisma } from "~/db.server";
 import { BIRTHDATE } from "~/secrets/constants";
@@ -33,6 +35,48 @@ export async function getPosts() {
   });
 }
 
+export async function getPostsChunkedByMonth() {
+  const posts = await prisma.post.findMany({
+    take: 60, // getting all of the 800+ posts is too much...
+    orderBy: {
+      dateSlug: "desc",
+    },
+  });
+
+  type Accumulator = Record<
+    string,
+    Array<Omit<AugmentedPost, "createdAt" | "updatedAt">>
+  >;
+  const postsGroupedByMonth = posts.reduce(
+    (accumulator: Accumulator, post: Post) => {
+      // want to get [ [2020-01-20, 20-01-21, ...], ..., [2022-05-01, 2022-05-02, ...]]
+      // start with {"2020-01": [2020-01-20, 20-01-21, ...], ..., "2022-05": [2022-05-01, 2022-05-02, ...]}
+      const dateStr = post.dateSlug; // eg. "2020-01-20"
+      const yearAndMonthStr = dateStr.substring(0, 7); // gets "2020-01"
+      const postDate = parseISO(dateStr);
+
+      accumulator[yearAndMonthStr] = [
+        ...(accumulator[yearAndMonthStr]?.length > 0
+          ? accumulator[yearAndMonthStr]
+          : []),
+        {
+          dateSlug: post.dateSlug,
+          title: post.title,
+          featuredImage: post.featuredImage,
+          markdown: post.markdown,
+          daysSinceBirth: differenceInCalendarDays(postDate, birthdate),
+          formattedDate: format(postDate, "EEEE, LLLL do, y"), // e.g. "Wednesday, May 25th, 2022"
+        },
+      ];
+
+      return accumulator;
+    },
+    {}
+  );
+
+  return postsGroupedByMonth;
+}
+
 export async function getPostsSortedByDescendingDate() {
   const posts = await prisma.post.findMany({
     orderBy: {
@@ -52,13 +96,111 @@ export async function getPostsSortedByDescendingDate() {
   });
 }
 
-export async function getPostsSinceBeginningOfLastMonthSortedByDescendingDate() {
+export async function getPostsSinceBeginningOfPreviousMonthSortedByDescendingDate() {
   const today = new Date();
   const prevMonth = today.getMonth() - 1;
   const firstDayOfMonth = 1;
   // Because of how dates work in JavaScript, the value of the year gets rolled back if we subtract 1 month from January.
   const firstDayPreviousMonth = new Date(
     today.getFullYear(),
+    prevMonth,
+    firstDayOfMonth
+  );
+  const dateSlugOfFirstDayPreviousMonth = formatISO(firstDayPreviousMonth, {
+    representation: "date",
+  }); // to get date string in format "YYYY-MM-DD"
+  const posts = await prisma.post.findMany({
+    // take: 10,
+    orderBy: {
+      dateSlug: "desc",
+    },
+    where: {
+      dateSlug: {
+        gte: dateSlugOfFirstDayPreviousMonth,
+      },
+    },
+  });
+  return posts.map((post) => {
+    const postDate = parseISO(post.dateSlug);
+    return {
+      dateSlug: post.dateSlug,
+      title: post.title,
+      featuredImage: post.featuredImage,
+      markdown: post.markdown,
+      daysSinceBirth: differenceInCalendarDays(postDate, birthdate),
+      formattedDate: format(postDate, "EEEE, LLLL do, y"), // e.g. "Wednesday, May 25th, 2022"
+    };
+  });
+}
+
+export async function getPostsSinceBeginningOfMonthOfLastPostDateSortedByDescendingDate() {
+  const postsArrayContainingLatestPostOnly = await prisma.post.findMany({
+    take: 1,
+    orderBy: {
+      dateSlug: "desc",
+    },
+  });
+  invariant(
+    head(postsArrayContainingLatestPostOnly),
+    "No posts returned from getPostsSinceBeginningOfMonthOfLastPostDateSortedByDescendingDate"
+  );
+  const dateOfLatestPost = new Date(
+    head(postsArrayContainingLatestPostOnly)!.dateSlug
+  );
+  const prevMonth = dateOfLatestPost.getMonth();
+  const firstDayOfMonth = 1;
+  // Because of how dates work in JavaScript, the value of the year gets rolled back if we subtract 1 month from January.
+  const firstDayPreviousMonth = new Date(
+    dateOfLatestPost.getFullYear(),
+    prevMonth,
+    firstDayOfMonth
+  );
+  const dateSlugOfFirstDayPreviousMonth = formatISO(firstDayPreviousMonth, {
+    representation: "date",
+  }); // to get date string in format "YYYY-MM-DD"
+  const posts = await prisma.post.findMany({
+    // take: 10,
+    orderBy: {
+      dateSlug: "desc",
+    },
+    where: {
+      dateSlug: {
+        gte: dateSlugOfFirstDayPreviousMonth,
+      },
+    },
+  });
+  return posts.map((post) => {
+    const postDate = parseISO(post.dateSlug);
+    return {
+      dateSlug: post.dateSlug,
+      title: post.title,
+      featuredImage: post.featuredImage,
+      markdown: post.markdown,
+      daysSinceBirth: differenceInCalendarDays(postDate, birthdate),
+      formattedDate: format(postDate, "EEEE, LLLL do, y"), // e.g. "Wednesday, May 25th, 2022"
+    };
+  });
+}
+
+export async function getPostsSinceBeginningOfMonthPreviousToLastPostDateSortedByDescendingDate() {
+  const postsArrayContainingLatestPostOnly = await prisma.post.findMany({
+    take: 1,
+    orderBy: {
+      dateSlug: "desc",
+    },
+  });
+  invariant(
+    head(postsArrayContainingLatestPostOnly),
+    "No posts returned from getPostsSinceBeginningOfMonthPreviousToLastPostDateSortedByDescendingDate"
+  );
+  const dateOfLatestPost = new Date(
+    head(postsArrayContainingLatestPostOnly)!.dateSlug
+  );
+  const prevMonth = dateOfLatestPost.getMonth() - 1;
+  const firstDayOfMonth = 1;
+  // Because of how dates work in JavaScript, the value of the year gets rolled back if we subtract 1 month from January.
+  const firstDayPreviousMonth = new Date(
+    dateOfLatestPost.getFullYear(),
     prevMonth,
     firstDayOfMonth
   );
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,24 +1,26 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import { format } from "date-fns";
 
 import {
   // getPosts,
-  getPostsSinceBeginningOfLastMonthSortedByDescendingDate,
+  // getPostsSinceBeginningOfPreviousMonthSortedByDescendingDate,
+  // getPostsSinceBeginningOfMonthPreviousToLastPostDateSortedByDescendingDate,
+  // getPostsSinceBeginningOfMonthOfLastPostDateSortedByDescendingDate,
+  getPostsChunkedByMonth,
   // getPostsSortedByDescendingDate,
 } from "~/models/post.server";
 import { AWS_PHOTO_BUCKET_URL } from "~/secrets/constants";
 import type { AugmentedPost } from "~/models/post.server";
 
 type LoaderData = {
-  posts: Awaited<
-    ReturnType<typeof getPostsSinceBeginningOfLastMonthSortedByDescendingDate>
-  >;
+  posts: Awaited<ReturnType<typeof getPostsChunkedByMonth>>;
 };
 
 export const loader = async () => {
   return json<LoaderData>({
-    posts: await getPostsSinceBeginningOfLastMonthSortedByDescendingDate(),
+    posts: await getPostsChunkedByMonth(),
   });
 };
 
@@ -29,46 +31,58 @@ function PostItem({
 }) {
   const [loading, setLoading] = useState(true);
 
-  console.log("image", post.dateSlug, "loading:", loading);
-
   return (
-    <div className="mb-2 w-1/2 px-1 md:mb-4 md:w-1/3 md:px-2 lg:mb-6 lg:w-1/4 lg:px-3 xl:mb-8 xl:w-1/5 xl:px-4">
-      <Link to={post.dateSlug}>
-        <div
-          className={
-            loading ? "mx-auto w-full max-w-sm rounded shadow-md" : "hidden"
-          }
-        >
-          <div className="flex animate-pulse space-x-4">
-            <div className="aspect-[3/4] w-full bg-slate-300"></div>
+    <>
+      <div className="mb-2 w-1/2 px-1 md:mb-4 md:w-1/3 md:px-2 lg:mb-6 lg:w-1/4 lg:px-3 xl:mb-8 xl:w-1/5 xl:px-4">
+        <Link to={post.dateSlug}>
+          <div
+            className={
+              loading ? "mx-auto w-full max-w-sm rounded shadow-md" : "hidden"
+            }
+          >
+            <div className="flex animate-pulse space-x-4">
+              <div className="aspect-[3/4] w-full bg-slate-300"></div>
+            </div>
           </div>
-        </div>
-        <img
-          alt={post.title}
-          className={loading ? "hidden" : "fadeIn1s rounded shadow-md"}
-          onLoad={() => setLoading(false)}
-          ref={(imageElement: HTMLImageElement) => {
-            if (imageElement && imageElement.complete) setLoading(false);
-          }}
-          src={`${AWS_PHOTO_BUCKET_URL}${post.featuredImage}`}
-        />
-        {/* {post.title} - {post.dateSlug} - {post.daysSinceBirth} -{" "}
+          <img
+            alt={post.title}
+            className={loading ? "hidden" : "fadeIn1s rounded shadow-md"}
+            onLoad={() => setLoading(false)}
+            ref={(imageElement: HTMLImageElement) => {
+              if (imageElement && imageElement.complete) setLoading(false);
+            }}
+            src={`${AWS_PHOTO_BUCKET_URL}${post.featuredImage}`}
+          />
+          {/* {post.title} - {post.dateSlug} - {post.daysSinceBirth} -{" "}
         {post.formattedDate} */}
-      </Link>
-    </div>
+        </Link>
+      </div>
+    </>
   );
 }
 
 export default function Posts() {
   const { posts } = useLoaderData<LoaderData>();
+
   return (
     <main className="container mx-auto">
-      <section className="">
-        <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <PostItem post={post} key={post.dateSlug} />
-          ))}
-        </div>
+      <section>
+        {Object.keys(posts).map((month) => {
+          const date = new Date(month);
+          return (
+            <div key={month}>
+              <div className="flex">
+                <div className="mr-1">{format(date, "MMMM")}</div>
+                <div>{format(date, "YYY")}</div>
+              </div>
+              <div className="flex flex-wrap">
+                {posts[month].map((post) => (
+                  <PostItem post={post} key={post.dateSlug} />
+                ))}
+              </div>
+            </div>
+          );
+        })}
       </section>
     </main>
   );
